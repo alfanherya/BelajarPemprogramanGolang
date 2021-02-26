27e6c50aa684b6077499c73495425faab11c7221
alfanherya <alfanherya@gmail.com>
2021-02-26 23:42:09 +0700

    menambahkan variable dan function belajar

--- hello-world/main.go
+++ hello-world/main.go
@@ -19,10 +19,16 @@ func handlerHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(message))
 }
 
+func handleBelajar(w http.ResponseWriter, r *http.Request) {
+	var pesan = "saya menggunakan variabel pakai bahasa indonesia"
+	w.Write([]byte(pesan))
+}
+
 func main() {
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
+	http.HandleFunc("/belajar", handleBelajar)
 
 	var address = "localhost:8989"
 	fmt.Printf("server started at %s\n", address)
