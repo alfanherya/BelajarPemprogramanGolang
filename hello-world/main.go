dad2661cd813e96323d870cb7e109a47b5105306
alfanherya <alfanherya@gmail.com>
2021-02-26 23:32:07 +0700

    coding pertama golang

---
+++ hello-world/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// func main() {
+// 	fmt.Println("Belajar coding golang melanjutkan mempelajari golang")
+// }
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	var message = "Ini adalah web App pertama saya"
+	w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	var message = "Hello saya sedang belajar"
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var address = "localhost:8989"
+	fmt.Printf("server started at %s\n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
